598ba491058ea746d01de1c3267c1693aa4bea31
Marc Dibold <marc.dibold@gmail.com>
2017-06-02 10:53:38 +0200

    Fix `Implementations of Accounts.ui.Field must render a message`. Fix `The use of FormMessage in Form has been deprecated` Warning.

--- main.jsx
+++ main.jsx
@@ -18,7 +18,7 @@ class Form extends Accounts.ui.Form {
       fields,
       buttons,
       error,
-      message,
+      messages,
       ready = true,
       className,
       formState
@@ -34,7 +34,7 @@ class Form extends Accounts.ui.Form {
           </div>
         ): null }
         {_.values(_.omit(buttons, 'switchToPasswordReset', 'switchToSignIn',
-          'switchToSignUp', 'switchToChangePassword', 'switchToSignOut', 'signOut')).map((button, i) =>
+        'switchToSignUp', 'switchToChangePassword', 'switchToSignOut', 'signOut')).map((button, i) =>
           <Button {...button} key={i} />
         )}
         { buttons['signOut'] ? (
@@ -58,9 +58,13 @@ class Form extends Accounts.ui.Form {
           </div>
         ) : null }
         { formState == STATES.SIGN_IN || formState == STATES.SIGN_UP ? (
-            <Accounts.ui.SocialButtons oauthServices={ oauthServices } />
+          <Accounts.ui.SocialButtons oauthServices={ oauthServices } />
         ) : null }
-        <Accounts.ui.FormMessage className="ui message" style={{display: 'block'}} {...message} />
+        <Accounts.ui.FormMessage
+          className="ui message"
+          style={{ display: 'block' }}
+          {...messages}
+        />
       </form>
     );
   }
@@ -79,23 +83,29 @@ class Button extends Accounts.ui.Button {
       icon
     } = this.props;
     return type == 'link' ? (
-      <a href={ href }
-         style={{cursor: 'pointer'}}
-         className={ className }
-         onClick={ onClick }>{ icon ? (<i className={["icon", icon].join(' ')} />) : null }{ label }</a>
+      <a
+        href={href}
+        style={{cursor: 'pointer'}}
+        className={className}
+        onClick={onClick}
+      >
+        {icon && (<i className={["icon", icon].join(' ')} />)}{label}
+      </a>
     ) : (
-      <button className={ [
-                'ui button',
-                type == 'submit' ? 'primary' : '',
-                disabled ? 'disabled' : '',
-                className
-              ].join(' ') }
-              type={ type } 
-              disabled={ disabled }
-              onClick={ onClick }>{ icon ? (<i className={["icon", icon].join(' ')} />) : null }{ label }</button>
+      <button
+        className={[
+          'ui button',
+          type == 'submit' ? 'primary' : '',
+          disabled ? 'disabled' : '',
+          className
+          ].join(' ')}
+        type={type} 
+        disabled={disabled}
+        onClick={onClick}>{icon && (<i className={["icon", icon].join(' ')} />)}{label}</button>
     );
   }
 }
+
 class Fields extends Accounts.ui.Fields {
   render () {
     let { fields = {}, className = "field" } = this.props;
@@ -108,6 +118,7 @@ class Fields extends Accounts.ui.Fields {
     );
   }
 }
+
 class Field extends Accounts.ui.Field {
   render() {
     const {
@@ -118,7 +129,8 @@ class Field extends Accounts.ui.Field {
       onChange,
       required = false,
       className,
-      defaultValue = ""
+      defaultValue = "",
+      message
     } = this.props;
     const { mount = true } = this.state;
     return mount ? (
@@ -127,37 +139,44 @@ class Field extends Accounts.ui.Field {
         <div className="ui fluid input">
           <input id="password" name="password" style={{display: 'none'}} />
           <input id={ id }
-                 name={ id }
-                 type={ type }
-                 ref={ (ref) => this.input = ref }
-                 autoCapitalize={ type == 'email' ? 'none' : false }
-                 autoCorrect="off"
-                 onChange={ onChange }
-                 placeholder={ hint } defaultValue={ defaultValue } />
+            name={ id }
+            type={ type }
+            ref={ (ref) => this.input = ref }
+            autoCapitalize={ type == 'email' ? 'none' : false }
+            autoCorrect="off"
+            onChange={ onChange }
+            placeholder={ hint } defaultValue={ defaultValue } />
         </div>
+        {message && (
+          <span className={['message', message.type].join(' ').trim()}>
+            {message.message}
+          </span>
+        )}
       </div>
     ) : null;
   }
 }
+
 export class PasswordOrService extends Accounts.ui.PasswordOrService {
   render() {
-    let { className, style = {} } = this.props;
-    let { hasPasswordService, services } = this.state;
-    labels = services;
+    const { className, style = {} } = this.props;
+    const { hasPasswordService, services } = this.state;
+    let labels = services;
     if (services.length > 2) {
       labels = [];
     }
 
     if (hasPasswordService && services.length > 0) {
       return (
-        <p style={ style } className={ className }>
-          { `${T9n.get('orUse')} ${ labels.join(' / ') }` }
+        <p style={style} className={className}>
+          {`${T9n.get('orUse')} ${ labels.join(' / ') }`}
         </p>
       );
     }
     return null;
   }
 }
+
 class SocialButtons extends Accounts.ui.SocialButtons {
   render() {
     let { oauthServices = {}, className = "social-buttons" } = this.props;
@@ -165,22 +184,25 @@ class SocialButtons extends Accounts.ui.SocialButtons {
       <div className={ className }>
         {Object.keys(oauthServices).map((id, i) => {
           var mapObj = {
-             google:"google plus",
-             "meteor-developer": ""
+            google:"google plus",
+            "meteor-developer": ""
           };
           let serviceClass = id.replace(/google|meteor\-developer/gi, (matched) => {
             return mapObj[matched];
           });
           return (
-            <Accounts.ui.Button key={i}
-                                className={["ui button", serviceClass].join(' ')}
-                                icon={serviceClass} {..._.omit(oauthServices[id], "className")} />
+            <Accounts.ui.Button
+              key={i}
+              className={["ui button", serviceClass].join(' ')}
+              icon={serviceClass} {..._.omit(oauthServices[id], "className")}
+            />
           );
         })}
       </div>
     );
   }
 }
+
 class FormMessage extends Accounts.ui.FormMessage {}
 // Notice! Accounts.ui.LoginForm manages all state logic at the moment, so avoid
 // overwriting this one, but have a look at it and learn how it works. And pull
